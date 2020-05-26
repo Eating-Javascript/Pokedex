0ec63cbde7f86a2220584aec03c1d1050b96a061
Lena <lenasurlenet@gmail.com>
2020-05-26 13:30:53 +0200

    css

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ const generateHTML = (data) => {
   <div class="name text-3xl text-green-400 font-bold text-center"> ${data.name}</div>
     <img class="w-full" src="${data.sprites.front_default}">
     <div class="details text-center px-6 py-4">
-      <span class="type inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">${data.type}</span>
-      <span class="type inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#${data.type}</span>
+      <span class="type inline-block bg-gray-200 rounded-sm px-3 py-1 text-sm font-semibold text-gray-700 mr-2">${data.type}</span>
+      <span class="type inline-block bg-gray-200 rounded-sm px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#${data.type}</span>
     </div>
   </div>
   `
